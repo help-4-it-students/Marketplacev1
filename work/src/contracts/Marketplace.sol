15e62a8dd286047584ea781d2aa446e382fc3707
help-4-it-students <70287872+help-4-it-students@users.noreply.github.com>
2023-05-23 11:02:43 +0400

    go
    go

--- work/src/contracts/Marketplace.sol
+++ work/src/contracts/Marketplace.sol
@@ -99,16 +99,35 @@ contract Marketplace {
         );
         productTransactions[_id].push(newTransaction);
 
-        emit ProductPurchased(
-            _id,
-            _product.name,
-            _product.nameimg,
-            _product.desc,
-            _product.price,
-            payable(msg.sender),
-            true
-        );
+ 
     }
+
+
+    function purchaseProduct(uint _id) public payable {
+        // Fetch the product
+        Product memory _product = products[_id];
+        // Fetch the owner
+        address payable _seller = _product.owner;
+        // Make sure the product has a valid id
+        require(_product.id > 0 && _product.id <= productCount);
+        // Require that there is enough Ether in the transaction
+        require(msg.value >= _product.price);
+        // Require that the product has not been purchased already
+        require(!_product.purchased);
+        // Require that the buyer is not the seller
+        require(_seller != msg.sender);
+        // Transfer ownership to the buyer
+        _product.owner = payable(msg.sender);
+        // Mark as purchased
+        _product.purchased = true;
+        // Update the product
+        products[_id] = _product;
+        // Pay the seller by sending them Ether
+        payable(address(_seller)).transfer(msg.value);
+        // Trigger an event
+        emit ProductPurchased(productCount, _product.name,_product.nameimg,_product.desc, _product.price, payable(msg.sender), true);
+    }
+
 
     function editProduct(
         uint _id,
